591ed03adacb0302ae75fa67cf3aa131d544ac48
MOISES-DARLAN <moisesdarlan440@gmail.com>
2025-01-15 19:52:23 -0300

    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello, World!");
+        int[] numeros = new int[5];
+
+        for (int c=0; c<5; c++){
+            System.out.println(String.format("Qual numero na posição %d", c));
+            int numero = input.nextInt();
+            numeros[c] = numero;
+
+        }
+
+        System.out.println("busca linear");
+        System.out.print("Digite o numero que você quer achar? ");
+        int alvo = input.nextInt();
+        int posicao = -1;
+        for (int num = 0; num< numeros.length; num++){
+            if(numeros[num] == alvo){
+                posicao = num;
+                System.out.println(String.format("O item: %d foi achado na posição: %d", alvo, posicao));
+                return;
+            }
+        }
+
     }
+
+    public  void imprimeArray(int[] array){
+        for (int c: array){
+            System.out.print("[ "+ c + ", ");
+        }
+        System.out.print("]");
+    }
+
 }
